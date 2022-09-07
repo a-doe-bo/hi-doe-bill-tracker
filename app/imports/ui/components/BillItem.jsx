a12098763a983c7e56b3e93c9e92ce3b03927ca4
Gavin Peng <blackbullet54321@gmail.com>
2022-09-06 23:08:06 -1000

    Implement upcoming bills table

---
+++ app/imports/ui/components/BillItem.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { BookmarkPlusFill, CaretDownFill, CaretRightFill } from 'react-bootstrap-icons';
+import { Button, Collapse, Table } from 'react-bootstrap';
+import { COMPONENT_IDS } from '../utilities/ComponentIDs';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+const BillItem = ({ billData: { bill_name, bill_status, bill_number, bill_hearing, _id } }) => {
+  const [toggle, setToggle] = useState(true);
+  const [collapsableTable, setCollapsableTable] = useState(false);
+  return (
+    <>
+      <tr>
+        <td>
+          {collapsableTable ? (
+            <Button onClick={() => { setCollapsableTable(!collapsableTable); }} aria-expanded={collapsableTable} aria-controls="collapse-table">
+              <CaretDownFill size={40} />
+            </Button>
+          )
+            : (
+              <Button onClick={() => { setCollapsableTable(!collapsableTable); }} aria-expanded={collapsableTable} aria-controls="collapse-table">
+                <CaretRightFill size={40} />
+              </Button>
+            )}
+        </td>
+        <td>
+          <BookmarkPlusFill onClick={() => { setToggle(!toggle); }} size={50} fill={toggle ? '#c4c4c4' : '#E7D27C'} />
+        </td>
+        <td>{bill_number}</td>
+        <td>{bill_name}</td>
+        <td>{bill_status}</td>
+        <td>{bill_hearing}</td>
+        <td>
+          <Link className={COMPONENT_IDS.LIST_STUFF_EDIT} to={`/edit/${_id}`}>View Bill</Link>
+        </td>
+      </tr>
+      {/* Collapsable Table */}
+      <tr>
+        <td style={{ padding: 0 }}>
+          <Collapse in={collapsableTable}>
+            <div id="collapse-table">
+              <Table>
+                <thead>
+                  <tr>
+                    <th>Hearing Location</th>
+                    <th>Date Introduced</th>
+                    <th>Committee</th>
+                    <th>Committee</th>
+                    <th>Committee</th>
+                    <th>Committee</th>
+                    <th>Committee</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Some Data</td>
+                    <td>Some Data</td>
+                    <td>Some Data</td>
+                    <td>Some Data</td>
+                    <td>Some Data</td>
+                    <td>Some Data</td>
+                    <td>Some Data</td>
+                  </tr>
+                  <tr>
+                    <td>Some Data</td>
+                    <td>Some Data</td>
+                    <td>Some Data</td>
+                    <td>Some Data</td>
+                    <td>Some Data</td>
+                    <td>Some Data</td>
+                    <td>Some Data</td>
+                  </tr>
+                  <tr>
+                    <td>Some Data</td>
+                    <td>Some Data</td>
+                    <td>Some Data</td>
+                    <td>Some Data</td>
+                    <td>Some Data</td>
+                    <td>Some Data</td>
+                    <td>Some Data</td>
+                  </tr>
+                </tbody>
+              </Table>
+            </div>
+          </Collapse>
+        </td>
+      </tr>
+    </>
+  );
+};
+
+BillItem.propTypes = {
+  billData: PropTypes.shape({
+    _id: PropTypes.string,
+    bill_name: PropTypes.string,
+    bill_status: PropTypes.string,
+    bill_hearing: PropTypes.string,
+    bill_number: PropTypes.number,
+  }).isRequired,
+};
+
+export default BillItem;
--- app/imports/ui/pages/ListStuff.jsx
+++ app/imports/ui/pages/ListStuff.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Col, Container, Row, Table } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Stuffs } from '../../api/stuff/StuffCollection';
-import StuffItem from '../components/StuffItem';
+import BillItem from '../components/BillItem';
 import LoadingSpinner from '../components/LoadingSpinner';
 import { PAGE_IDS } from '../utilities/PageIDs';
 
-/* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
+/* Renders a table containing all of the Stuff documents. Use <BillItem> to render each row. */
 const ListStuff = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
   const { ready, stuffs } = useTracker(() => {
@@ -40,7 +40,7 @@ const ListStuff = () => {
               </tr>
             </thead>
             <tbody>
-              {stuffs.map((stuff) => <StuffItem key={stuff._id} stuff={stuff} />)}
+              {stuffs.map((stuff) => <BillItem key={stuff._id} stuff={stuff} />)}
             </tbody>
           </Table>
         </Col>
