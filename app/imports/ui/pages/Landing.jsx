2c472e7993992b43201e86029645d568df73d303
Cole House <30875198+Cole-House@users.noreply.github.com>
2022-09-15 11:31:07 -1000

    Landing Roughdraft

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -4,33 +4,35 @@ import { PAGE_IDS } from '../utilities/PageIDs';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
-  <Container className="landingContainer">
-    <Row className="align-middle text-center">
-      <Col>
-        <Image src="/images/aDOEbo-logo.png" width="250px" />
-        <br />
-        <br />
-        <p> DOE-Tracker is a student made web-application to assist the DOE in tracking important bills
-          through the legistlative process.
-        </p>
-      </Col>
+  <div className="landingContainer">
+    <Container id={PAGE_IDS.LANDING} className="py-3">
+      <Row className="align-middle text-center">
+        <Col>
+          <Image src="/images/aDOEbo-logo.png" width="250px" />
+          <br />
+          <br />
+          <p> DOE-Tracker is a student made web-application to assist the DOE in tracking important bills
+            through the legistlative process.
+          </p>
+        </Col>
 
-      <Col xs={8} className="d-flex flex-column justify-content-center">
-        <h1 className="landingHeader">Stay in the know with</h1>
-        <h1 className="landingHeader" style={{ color: '#3866f2' }}>DOE-Tracker!</h1>
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <br />
-        <br />
-        <br />
-        <br />
-        {/* eslint-disable-next-line jsx-a11y/no-redundant-roles */}
-        <a href="http://localhost:3000/signin"><button type="button" className="btn btn-outline-primary" href="http://localhost:3000/signin" role="button">Sign in here</button></a>
-        <br />
-        <a href="http://localhost:3000/signup">New? Signup Here</a>
-      </Col>
+        <Col xs={8} className="d-flex flex-column justify-content-center">
+          <h1 className="landingHeader">Stay in the know with</h1>
+          <h1 className="landingHeader" style={{ color: '#3866f2' }}>DOE-Tracker!</h1>
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <br />
+          <br />
+          <br />
+          <br />
+          {/* eslint-disable-next-line jsx-a11y/no-redundant-roles */}
+          <a href="http://localhost:3000/signin"><button type="button" className="btn btn-outline-primary" href="http://localhost:3000/signin" role="button">Sign in here</button></a>
+          <br />
+          <a href="http://localhost:3000/signup">New? Signup Here</a>
+        </Col>
 
-    </Row>
-  </Container>
+      </Row>
+    </Container>
+  </div>
 );
 
 export default Landing;
