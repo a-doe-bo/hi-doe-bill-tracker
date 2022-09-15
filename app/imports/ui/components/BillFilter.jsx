5546c0e96e11b2be09acd14d9da829541e121e10
Adrian Tam <65895954+adrianwtam@users.noreply.github.com>
2022-09-14 20:59:38 -1000

    Barebones Advanced Filter Search
    Will flesh it out later on

--- app/imports/ui/components/BillFilter.jsx
+++ app/imports/ui/components/BillFilter.jsx
@@ -23,35 +23,43 @@ const Example = () => {
         keyboard={false}
       >
         <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
+          <Modal.Title>Advanced Search</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form>
+            <Form.Group className="mb-3" controlId="formBillNumber">
+              <Form.Label>Bill/Resolution No #</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formOffice">
+              <Form.Label>Office Name</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We will never share your email with anyone else.
-              </Form.Text>
+              <Form.Label>Bill/Resolution No #</Form.Label>
+              <Form.Control type="text" />
             </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+            <Form.Group className="mb-3" controlId="formAction">
+              <Form.Label>Action</Form.Label>
+              <Form.Control type="text" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
+            <Form.Group className="mb-3" controlId="formStatus">
+              <Form.Label>Status</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formHearingDate">
+              <Form.Label>Hearing Date</Form.Label>
+              <Form.Control type="date" />
             </Form.Group>
             <Button variant="primary" type="submit">
-              Submit
+              Search
             </Button>
           </Form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
+          <Button variant="danger" onClick={handleClose}>
+            Cancel
           </Button>
-          <Button variant="primary">Understood</Button>
         </Modal.Footer>
       </Modal>
     </>
