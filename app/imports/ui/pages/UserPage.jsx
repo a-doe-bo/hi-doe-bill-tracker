d8890b91344b6ed52fbf2e0f44d9533cae1e664b
joshmcons <jmc921@hawaii.edu>
2022-09-15 18:45:35 -1000

    Update UserPage.jsx

--- app/imports/ui/pages/UserPage.jsx
+++ app/imports/ui/pages/UserPage.jsx
@@ -7,42 +7,13 @@ import { COMPONENT_IDS } from '../utilities/ComponentIDs';
 
 const UserPage = () => {
 
-  const schema = new SimpleSchema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    employeeID: String,
-    role: { type: String, allowedValues: ['SECRETARY', 'WRITER', 'OFFICE APPROVER', 'PIPE APPROVER', 'FINAL APPROVER'] },
-  });
-  const bridge = new SimpleSchema2Bridge(schema);
+    const user = {
+     name: 'Very Important Person',
+     title: 'DOE Public Affairs',
+     img: '',
+     priv: 'Secretary',
+    };
 
-  let fRef = null;
-  return (
-    // eslint-disable-next-line react/react-in-jsx-scope
-    <Container id={PAGE_IDS.USER_PAGE} className="py-3">
-      <Row className="justify-content-center">
-        <Col xs={5}>
-          <Col className="text-center">
-            <h2>User Page</h2>
-          </Col>
-          <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => submit(data, fRef)}>
-            <Card>
-              <Card.Body>
-                <TextField id={COMPONENT_IDS.SIGN_UP_FORM_FIRST_NAME} name="firstName" placeholder="First name" />
-                <TextField id={COMPONENT_IDS.SIGN_UP_FORM_LAST_NAME} name="lastName" placeholder="Last name" />
-                <TextField id={COMPONENT_IDS.SIGN_UP_FORM_EMAIL} name="email" placeholder="E-mail address" />
-                <TextField id={COMPONENT_IDS.SIGN_UP_EMPLOYEE_ID} name="employeeID" placeholder="Employee ID" />
-                <TextField id={COMPONENT_IDS.SIGN_UP_FORM_PASSWORD} name="password" placeholder="Password" type="password" />
-                <SelectField id={COMPONENT_IDS.SIGN_UP_FORM_ROLE} name="role" placeholder="Choose a role" />
-                <ErrorsField />
-                <SubmitField id={COMPONENT_IDS.SIGN_UP_FORM_SUBMIT} />
-              </Card.Body>
-            </Card>
-          </AutoForm>
-        </Col>
-      </Row>
-    </Container>
-  );
-};
+
 
 export default UserPage;
