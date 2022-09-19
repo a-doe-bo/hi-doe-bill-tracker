b70ff2fe4873cd1a5fe7559e14e54106f37b5ab6
Thaneluna <thanel@hawaii.edu>
2022-09-19 00:44:28 -1000

    Update SignOut.jsx

--- app/imports/ui/pages/SignOut.jsx
+++ app/imports/ui/pages/SignOut.jsx
@@ -4,7 +4,7 @@ import { Col } from 'react-bootstrap';
 import { PAGE_IDS } from '../utilities/PageIDs';
 
 /* After the user clicks the "SignOut" link in the NavBar, log them out and display this page. */
-export const SignOut = () => {
+const SignOut = () => {
   Meteor.logout();
   return (
     <Col id={PAGE_IDS.SIGN_OUT} className="text-center py-3">
@@ -12,3 +12,5 @@ export const SignOut = () => {
     </Col>
   );
 };
+
+export default SignOut;
