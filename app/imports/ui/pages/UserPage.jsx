5673465951a77750d6dfbc0be942fd2ce4301086
joshmcons <jmc921@hawaii.edu>
2022-09-21 00:30:25 -1000

    Fixed image issue and created horizontal card

--- app/imports/ui/pages/UserPage.jsx
+++ app/imports/ui/pages/UserPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '/client/style.css';
-import { Card, Container } from 'react-bootstrap';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 import { PAGE_IDS } from '../utilities/PageIDs';
 
 const UserPage = () => {
@@ -9,21 +9,25 @@ const UserPage = () => {
     name: 'Very Important Person',
     email: 'john@foo.com',
     title: 'DOE Public Affairs',
-    img: '/img/butterfly.png',
+    img: '/images/butterfly.png',
     role: 'Secretary',
   };
 
   return (
     <Container id={PAGE_IDS.USER_PROFILE} className="py-3">
-      <Card>
-        <Card.Img src={user.img} className="user-profile-pic" style={{ width: '24rem' }} />
-        <Card.Body className="text-left">
-          <Card.Title>Name: {user.name}</Card.Title>
-          <Card.Subtitle>Email: {user.email}</Card.Subtitle>
-          <Card.Text>Department: {user.title}</Card.Text>
-          <Card.Text>Role: {user.role}</Card.Text>
-        </Card.Body>
-      </Card>
+      <Row>
+        <Col>
+          <Card.Img src={user.img} className="col-auto img-thumbnail" style={{ width: '18rem' }} />
+        </Col>
+        <Col>
+          <Card.Body className="text-float-left">
+            <Card.Title>Name: {user.name}</Card.Title>
+            <Card.Subtitle>Email: {user.email}</Card.Subtitle>
+            <Card.Text>Department: {user.title}</Card.Text>
+            <Card.Text>Role: {user.role}</Card.Text>
+          </Card.Body>
+        </Col>
+      </Row>
     </Container>
   );
 };
