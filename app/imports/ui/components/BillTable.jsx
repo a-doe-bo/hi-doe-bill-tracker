228872fb5730bee0971b7e3009ccbce477b951da
Kobeyarai808 <kobeyarai@gmail.com>
2022-10-09 00:02:15 -1000

    Finished Pagination Implementation

--- app/imports/ui/components/BillTable.jsx
+++ app/imports/ui/components/BillTable.jsx
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Table } from 'react-bootstrap';
 import BillItem from './BillItem';
+import Pagination2 from './Pagination2';
 
-const BillTable = ({ billData, tableHeaders }) => (
-  <Table striped bordered hover>
-    <thead>
-      <tr>
-        {tableHeaders.map((tableHeader, index) => (
-          <th key={index}>{tableHeader}</th>
-        ))}
-      </tr>
-    </thead>
-    <tbody>
-      {billData.map((data, index) => <BillItem key={index} billData={data} />)}
-    </tbody>
-  </Table>
-);
+const BillTable = ({ billData, tableHeaders }) => {
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [billsPerPage] = useState(235);
+
+  const indexOfLastBill = currentPage * billsPerPage;
+  const indexOfFirstBill = currentPage - billsPerPage;
+  const currentBills = billData.splice(indexOfFirstBill, indexOfLastBill);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  return (
+    <div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            {tableHeaders.map((tableHeader, index) => (
+              <th key={index}>{tableHeader}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {currentBills.map((data, index) => <BillItem key={index} billData={data} />)}
+        </tbody>
+      </Table>
+      <Pagination2 billsPerPage={billsPerPage} totalBills={billData.length} paginate={paginate} />
+    </div>
+  );
+};
 
 BillTable.propTypes = {
   billData: PropTypes.arrayOf(PropTypes.shape({
