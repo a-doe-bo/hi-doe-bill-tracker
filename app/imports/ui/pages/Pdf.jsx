88b6149637a09de7fe521e291f15cdb257940b00
Li Liang <liangli@hawaii.edu>
2022-10-11 13:41:30 -1000

    finished pdf template

--- app/imports/ui/pages/Pdf.jsx
+++ app/imports/ui/pages/Pdf.jsx
@@ -5,6 +5,8 @@ import JsPDF from 'jspdf';
 class Pdf extends Component {
   pdfGenerate = () => {
     const doc = new JsPDF();
+    const logo = new Image();
+    logo.src = '/images/DOElogo.png';
     const today = new Date();
     const date = (today.getMonth() + 1) + '/' + today.getDate() + '/' + today.getFullYear();
     const time = today.getHours() + ':' + today.getMinutes();
@@ -12,28 +14,29 @@ class Pdf extends Component {
     const position = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
     doc.setFont('Times', 'bold');
     doc.setFontSize(14);
+    doc.addImage(logo, 'png', 15, 10, 25 , 25);
     doc.text(130, 20, 'Date: ');
     doc.text(130, 25, 'Time: ');
     doc.text(130, 30, 'Location: ');
     doc.text(130, 35, 'Committee: ');
     doc.text(15, 50, 'Department: ');
-    doc.text(15, 65, 'Testifier ');
-    doc.text(15, 80, 'Title of Bill ');
-    doc.text(15, 95, 'Purpose of Bill ');
-    doc.text(15, 130, 'Department\'s Position: ');
+    doc.text(15, 60, 'Testifier ');
+    doc.text(15, 70, 'Title of Bill ');
+    doc.text(15, 80, 'Purpose of Bill ');
+    doc.text(15, 100, 'Department\'s Position: ');
     doc.setFont('Times', 'roman');
     doc.text(145, 20, date);
     doc.text(145, 25, time);
     doc.text(153, 30, 'xxxxxxxxxxxxx');
     doc.text(157, 35, 'xxxxxxxxxxxxx');
     doc.text(65, 50, 'xxxxxxxxxxxxx');
-    doc.text(65, 65, 'xxxxxxxxxxxxx');
-    doc.text(65, 80, 'xxxxxxxxxxxxx');
+    doc.text(65, 60, 'xxxxxxxxxxxxx');
+    doc.text(65, 70, 'xxxxxxxxxxxxx');
     const splitTitle = doc.splitTextToSize(purpose, 120);
-    doc.text(65, 95, splitTitle);
+    doc.text(65, 80, splitTitle);
     const splitTitle1 = doc.splitTextToSize(position, 170);
-    doc.text(15, 140, splitTitle1);
-    doc.save('a.pdf');
+    doc.text(15, 110, splitTitle1);
+    window.open(doc.output('bloburl'), '_blank');
   };
 
   render() {
