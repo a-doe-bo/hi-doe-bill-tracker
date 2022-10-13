4b1bcf6a4d5b717f4bb50a0d186aee8b84cff54e
Li Liang <70745380+LiLiang808@users.noreply.github.com>
2022-10-13 12:08:45 -1000

    update pdf

--- app/imports/ui/pages/Pdf.jsx
+++ app/imports/ui/pages/Pdf.jsx
@@ -1,15 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Button } from 'react-bootstrap';
 import GeneratePDF from '../components/GeneratePDF';
 
-class Pdf extends Component {
-
-  render() {
-    return (
-      <div style={{ textAlign: 'center' }}><br />
-        <Button onClick={GeneratePDF}>Generate PDF</Button>
-      </div>
-    );
-  }
-}
+const Pdf = () => (
+  <div style={{ textAlign: 'center' }}><br />
+    <Button onClick={GeneratePDF}>Generate PDF</Button>
+  </div>
+);
 export default Pdf;
