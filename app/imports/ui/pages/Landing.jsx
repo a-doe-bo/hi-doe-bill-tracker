a8949cad35b673d9535d9be94846e3318cd59e39
joshmcons <jmc921@hawaii.edu>
2022-10-27 01:58:35 -1000

    Styling

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -6,30 +6,32 @@ import { PAGE_IDS } from '../utilities/PageIDs';
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
   <div className="landingContainer">
-    <Container id={PAGE_IDS.LANDING} className="py-3">
-      <Row className="align-middle text-center">
-        <Col>
-          <Image src="/images/logo-720x720.png" style={{ width: '350px', marginBottom: '50px' }} />
-          <br />
-          <br />
-          <h5 style={{ color: '#140E03' }}> DOE-Tracker is a student made web-application to assist the DOE in tracking important bills
-            through the legislative process.
-          </h5>
-        </Col>
+    <div style={{ backgroundImage: 'url("https://www.tripsavvy.com/thmb/QhnBGPOxnY9LcfLQelBdPVUfwq4=/1440x1080/smart/filters:no_upscale()/hawaii-state-capitol-exterior-56a3b8113df78cf7727edd91.jpg")' }}>
+      <Container id={PAGE_IDS.LANDING} className="py-3">
+        <Row className="align-middle text-center">
+          <Col>
+            <Image src="/images/logo-720x720.png" style={{ width: '350px', marginBottom: '50px' }} />
+            <br />
+            <br />
+            <h5 style={{ color: '#140E03' }}> DOE-Tracker is a student made web-application to assist the DOE in tracking important bills
+              through the legislative process.
+            </h5>
+          </Col>
 
-        <Col className="d-flex flex-column justify-content-center">
-          <h1 className="landingHeader" style={{ color: '#140E03' }}>Stay in the know with</h1>
-          <h1 className="landingHeader" style={{ color: '#ef2c2c', marginBottom: '50px' }}>DOE-Tracker!</h1>
-          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-          <h4 style={{ color: '#fab538' }}>Organize <House /> -
-            Notify <Bell /> -
-            Testify <EnvelopeOpen /> -
-            Approve <CheckAll />
-          </h4>
-        </Col>
+          <Col className="d-flex flex-column justify-content-center">
+            <h1 className="landingHeader" style={{ color: '#140E03' }}>Stay in the know with</h1>
+            <h1 className="landingHeader" style={{ color: '#ef2c2c', marginBottom: '50px' }}>DOE-Tracker!</h1>
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+            <h4 style={{ color: '#fab538' }}>Organize <House /> -
+              Notify <Bell /> -
+              Testify <EnvelopeOpen /> -
+              Approve <CheckAll />
+            </h4>
+          </Col>
 
-      </Row>
-    </Container>
+        </Row>
+      </Container>
+    </div>
   </div>
 );
 
