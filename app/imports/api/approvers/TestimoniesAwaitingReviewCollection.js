df131bbe2a8cd2be5bed93b9305384ad15e92f58
Cole House <30875198+Cole-House@users.noreply.github.com>
2022-11-09 17:31:25 -1000

    Create TestimoniesAwaitingReviewCollection.js

---
+++ app/imports/api/approvers/TestimoniesAwaitingReviewCollection.js
@@ -0,0 +1,89 @@
+import { Meteor } from 'meteor/meteor';
+import SimpleSchema from 'simpl-schema';
+import BaseCollection from '../base/BaseCollection';
+import { ROLE } from '../role/Role';
+
+export const expertPublications = {
+  experts: 'experts',
+};
+
+class TestimoniesAwaitingReviewCollection extends BaseCollection {
+  constructor() {
+    super('Experts', new SimpleSchema({
+      recipient: String,
+      bill_name: String,
+      bill_number: Number,
+      bill_status: String,
+      bill_hearing: String,
+    }));
+  }
+
+  define({ recipient, bill_number, bill_name, bill_status, bill_hearing }) {
+    if (this.isDefined({ recipient, bill_number, bill_name, bill_status, bill_hearing })) {
+      return this.findDoc({ recipient, bill_number, bill_name, bill_status, bill_hearing })._id;
+    }
+
+    const docID = this._collection.insert({ recipient, bill_number, bill_name, bill_status, bill_hearing });
+    return docID;
+
+  }
+
+  update(docID, { recipient, bill_number, bill_name, bill_status, bill_hearing }) {
+    const updateData = {};
+    if (recipient) {
+      updateData.recipient = recipient;
+    }
+    if (bill_number) {
+      updateData.bill_number = bill_number;
+    }
+    if (bill_name) {
+      updateData.bill_name = bill_name;
+    }
+    if (bill_status) {
+      updateData.bill_status = bill_status;
+    }
+    if (bill_hearing) {
+      updateData.bill_hearing = bill_hearing;
+    }
+
+    this._collection.update(docID, { $set: updateData });
+  }
+
+  publish() {
+    if (Meteor.isServer) {
+      // get the MeasureCollection instance
+      const instance = this;
+      Meteor.publish(expertPublications.experts, function publish() {
+        if (this.userId) {
+          return instance._collection.find({});
+        }
+        return this.ready();
+      });
+    }
+  }
+
+  subscribeToExpert() {
+    if (Meteor.isClient) {
+      return Meteor.subscribe(expertPublications.experts);
+    }
+    return null;
+  }
+
+  assertValidRoleForMethod(userId) {
+    this.assertRole(userId, [ROLE.SECRETARY]);
+  }
+
+  dumpOne(docID) {
+    const doc = this.findDoc(docID);
+    const recipient = doc.recipient;
+    const bill_number = doc.bill_number;
+    const bill_name = doc.bill_name;
+    const bill_status = doc.bill_status;
+    const bill_hearing = doc.bill_hearing;
+
+    return { recipient, bill_number, bill_name, bill_status, bill_hearing };
+  }
+
+}
+
+export const Experts = new TestimoniesAwaitingReviewCollection();
