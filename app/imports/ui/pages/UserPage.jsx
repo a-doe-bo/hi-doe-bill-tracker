7988ed0921d17e8919361a027071132bb12ed57d
Adrian Tam <65895954+adrianwtam@users.noreply.github.com>
2022-11-09 16:47:08 -1000

    works
    but now we need to fix data mismatch between settings.development.json and UserProfileCollections

--- app/imports/ui/pages/UserPage.jsx
+++ app/imports/ui/pages/UserPage.jsx
@@ -1,35 +1,48 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
 import '/client/style.css';
 import { Card, Col, Container, Row } from 'react-bootstrap';
+import { useTracker } from 'meteor/react-meteor-data';
 import { PAGE_IDS } from '../utilities/PageIDs';
+import { UserProfiles } from '../../api/user/UserProfileCollection';
+import LoadingSpinner from '../components/LoadingSpinner';
+import userProfileData from '../components/UserProfileData';
 
 const UserPage = () => {
+  const _id = Meteor.userId();
+  console.log(_id);
+  const { ready, data } = useTracker(() => {
+    // Get access to Stuff documents.
+    const subscription = UserProfiles.subscribeUser();
+    // Determine if the subscription is ready
+    const rdy = subscription.ready();
+    // Get the document
+    const profileItem = UserProfiles.find({ userID: _id }, {}).fetch();
+    console.log(profileItem);
 
-  const user = {
-    name: 'John Foo',
-    email: 'john@foo.com',
-    title: 'DOE Public Affairs',
-    img: '/images/butterfly.png',
-    role: 'Secretary',
-  };
+    return {
+      data: profileItem[0],
+      ready: rdy,
+    };
+  });
 
-  return (
+  return ready ? (
     <Container id={PAGE_IDS.USER_PROFILE} className="py-3">
       <Row>
         <Col>
-          <Card.Img src={user.img} className="col-auto img-thumbnail" style={{ width: '18rem' }} />
+          <Card.Img src="/images/butterfly.png" className="col-auto img-thumbnail" style={{ width: '18rem' }} />
         </Col>
         <Col>
           <Card.Body className="text-float-left">
-            <Card.Title>Name: {user.name}</Card.Title>
-            <Card.Subtitle>Email: {user.email}</Card.Subtitle>
-            <Card.Text>Department: {user.title}</Card.Text>
-            <Card.Text>Role: {user.role}</Card.Text>
+            <Card.Title>Name: {data.firstName} {data.lastName} </Card.Title>
+            <Card.Subtitle>Email: {data.email}</Card.Subtitle>
+            <Card.Text>Department: {data.title}</Card.Text>
+            <Card.Text>Role: {data.role}</Card.Text>
           </Card.Body>
         </Col>
       </Row>
     </Container>
-  );
+  ) : <LoadingSpinner />;
 };
 
 export default UserPage;
