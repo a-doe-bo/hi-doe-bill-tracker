7708be639406e5a9f1904d43e66f1c400980b1ec
Thaneluna <thanel@hawaii.edu>
2022-11-10 15:38:46 -1000

    Updated PrimaryOfficeMeasure Error

--- app/imports/api/office/PrimaryOfficeMeasure.js
+++ app/imports/api/office/PrimaryOfficeMeasure.js
@@ -1,7 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import SimpleSchema from 'simpl-schema';
 import BaseCollection from '../base/BaseCollection';
-import { isValidOfficeType } from '../legislature/officeTypes';
 
 export const PrimaryOfficePublications = {
   PrimaryOffice: 'PrimaryOffice',
