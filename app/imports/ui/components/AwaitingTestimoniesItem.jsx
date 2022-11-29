7bf2047327a11f2184ff9974234240172c58afe7
joshmcons <jmc921@hawaii.edu>
2022-11-29 00:52:30 -1000

    Testimony

--- app/imports/ui/components/AwaitingTestimoniesItem.jsx
+++ app/imports/ui/components/AwaitingTestimoniesItem.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router';
+import { Roles } from 'meteor/alanning:roles';
+import { Meteor } from 'meteor/meteor';
 import { COMPONENT_IDS } from '../utilities/ComponentIDs';
-
+import { ROUTE_PATHS } from '../utilities/RoutePaths';
+import { ROLE } from '../../api/role/Role';
 // eslint-disable-next-line arrow-body-style
 const AwaitingTestimoniesItem = ({ awaitingTestimonies: { bill_name, bill_due_date, office, _id }, createDraft }) => {
+
+  const { pathname } = useLocation();
+
   return (
     <tr>
       <td>{bill_name}</td>
@@ -13,9 +20,14 @@ const AwaitingTestimoniesItem = ({ awaitingTestimonies: { bill_name, bill_due_da
       <td>
         <Link className={COMPONENT_IDS.VIEW_BILL} to={`/bill/${_id}`}>View Bill</Link>
       </td>
+      {(pathname === ROUTE_PATHS.DRAFT_TESTIMONY && Roles.userIsInRole(Meteor.userId(), [ROLE.WRITER])) && (
+        <td>
+          <Link className={COMPONENT_IDS.CREATE_DRAFT} to={`/draftTestimony/${_id}`}>Create Draft</Link>
+        </td>
+      )}
       {createDraft && (
         <td>
-          <Link className={COMPONENT_IDS.CREATE_DRAFT} to={`/createDraft/${_id}`}>Create Draft</Link>
+          <Link className={COMPONENT_IDS.CREATE_DRAFT} to={`/draftTestimony/${_id}`}>Create Draft</Link>
         </td>
       )}
     </tr>
--- app/imports/ui/utilities/RoutePaths.js
+++ app/imports/ui/utilities/RoutePaths.js
@@ -2,4 +2,5 @@ export const ROUTE_PATHS = {
   SAVED_BILLS: '/savedBills',
   LIST_AWAITING_REVIEWS: '/listAwaitingReviews',
   LIST_AWAITING_TESTIMONIES: '/listTestimonies',
+  DRAFT_TESTIMONY: '/draftTestimony',
 };
