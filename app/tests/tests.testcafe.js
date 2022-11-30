29f703294646e2642de2233f78d5feda8b6fd861
Kobeyarai808 <kobeyarai@gmail.com>
2022-11-29 22:09:16 -1000

    Adds test-cafe for saved bills

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -49,6 +49,17 @@ test('Test that user pages show up', async () => {
   await signOutPage.isDisplayed();
 });
 
+test('Test that saved bills pages show up', async () => {
+  await navBar.gotoSignInPage();
+  await signInPage.signin(credentials.username, credentials.password);
+  await navBar.isLoggedIn(credentials.username);
+  await navBar.gotoSavedBillsPage();
+  await listSavedBillsPage.isDisplayed();
+  // want to see if we can get to the editStuffPage
+  await navBar.logout();
+  await signOutPage.isDisplayed();
+});
+
 test('Test that admin pages show up', async () => {
   await navBar.gotoSignInPage();
   await signInPage.signin(adminCredentials.username, adminCredentials.password);
